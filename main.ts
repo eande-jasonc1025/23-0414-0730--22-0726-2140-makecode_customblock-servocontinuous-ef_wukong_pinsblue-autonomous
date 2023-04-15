235eb256730da6a6ce659047092024ed35ad78be
eande-jasonc1025 <67285598+eande-jasonc1025@users.noreply.github.com>
2023-04-15 07:53:37 -0700

    23-0415-0800 same as before

--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    quest_Autonomous.rq_Run_PID_Motion_Fn(
+    q_Autonomous.rq_Run_PID_Motion_Fn(
     0,
     3
     )
-    quest_Timer.rq_Set_ContinueCurrentState_CountdownTimer_Fn(2, rq_Time_Units_Enum.Seconds)
-    quest_Autonomous.rq_Run_PID_Motion_Fn(
+    q_Timer.rq_Set_ContinueCurrentState_CountdownTimer_Fn(2, rq_Time_Units_Enum.Seconds)
+    q_Autonomous.rq_Run_PID_Motion_Fn(
     90,
     3
     )
-    quest_Timer.rq_Set_ContinueCurrentState_CountdownTimer_Fn(2, rq_Time_Units_Enum.Seconds)
-    quest_Autonomous.rq_Run_PID_Motion_Fn(
+    q_Timer.rq_Set_ContinueCurrentState_CountdownTimer_Fn(2, rq_Time_Units_Enum.Seconds)
+    q_Autonomous.rq_Run_PID_Motion_Fn(
     90,
     3
     )
-    quest_Timer.rq_Set_ContinueCurrentState_CountdownTimer_Fn(2, rq_Time_Units_Enum.Seconds)
-    quest_Autonomous.rq_Run_PID_Motion_Fn(
+    q_Timer.rq_Set_ContinueCurrentState_CountdownTimer_Fn(2, rq_Time_Units_Enum.Seconds)
+    q_Autonomous.rq_Run_PID_Motion_Fn(
     90,
     3
     )
-    quest_Hardware.rq_Set_PowerMotorsViaBlueRedBlackPins_Fn(
+    q_Hardware.rq_Set_PowerMotorsViaBlueRedBlackPins_Fn(
     rq_PortGroup_BlueRedBlack_PortIds_Enum.S1_MotorLeft__S0_MotorRight,
     0,
     0
@@ -26,7 +26,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 })
 basic.showIcon(IconNames.Happy)
 basic.pause(2000)
-quest_Autonomous.rq_Set_PID_Settings_Fn(
+q_Autonomous.rq_Set_PID_Settings_Fn(
 0.2,
 0.15,
 0.015,
